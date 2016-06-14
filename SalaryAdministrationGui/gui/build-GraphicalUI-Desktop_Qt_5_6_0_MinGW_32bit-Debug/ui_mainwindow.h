70097b3fb4abf86b32bd47810b1571a80da8443b
akeskimo <aapokesk@gmail.com>
2016-06-14 22:22:37 +0300

    Add files via upload
    Initial commit

---
+++ SalaryAdministrationGui/gui/build-GraphicalUI-Desktop_Qt_5_6_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,234 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QComboBox *comboBox;
+    QLabel *label;
+    QPlainTextEdit *textName;
+    QLabel *label_2;
+    QPlainTextEdit *textMonthlyCompensation;
+    QLabel *label_3;
+    QPlainTextEdit *textSocialSecurityNumber;
+    QLabel *label_4;
+    QPlainTextEdit *textDoneHours;
+    QLabel *label_5;
+    QPlainTextEdit *textHourlyCompensation;
+    QLabel *label_6;
+    QPlainTextEdit *textRealizedOutcome;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QCheckBox *checkBox;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+    QMenuBar *menuBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(412, 520);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        comboBox = new QComboBox(centralWidget);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setGeometry(QRect(10, 30, 151, 22));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(10, 10, 111, 16));
+        textName = new QPlainTextEdit(centralWidget);
+        textName->setObjectName(QStringLiteral("textName"));
+        textName->setGeometry(QRect(10, 90, 104, 31));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(10, 70, 111, 16));
+        textMonthlyCompensation = new QPlainTextEdit(centralWidget);
+        textMonthlyCompensation->setObjectName(QStringLiteral("textMonthlyCompensation"));
+        textMonthlyCompensation->setGeometry(QRect(10, 210, 104, 31));
+        QPalette palette;
+        QBrush brush(QColor(0, 0, 0, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        QBrush brush1(QColor(0, 170, 255, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Button, brush1);
+        QBrush brush2(QColor(127, 213, 255, 255));
+        brush2.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Light, brush2);
+        QBrush brush3(QColor(63, 191, 255, 255));
+        brush3.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Midlight, brush3);
+        QBrush brush4(QColor(0, 85, 127, 255));
+        brush4.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Dark, brush4);
+        QBrush brush5(QColor(0, 113, 170, 255));
+        brush5.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Mid, brush5);
+        palette.setBrush(QPalette::Active, QPalette::Text, brush);
+        QBrush brush6(QColor(255, 255, 255, 255));
+        brush6.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::BrightText, brush6);
+        palette.setBrush(QPalette::Active, QPalette::ButtonText, brush);
+        palette.setBrush(QPalette::Active, QPalette::Base, brush6);
+        palette.setBrush(QPalette::Active, QPalette::Window, brush1);
+        palette.setBrush(QPalette::Active, QPalette::Shadow, brush);
+        QBrush brush7(QColor(127, 212, 255, 255));
+        brush7.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::AlternateBase, brush7);
+        QBrush brush8(QColor(255, 255, 220, 255));
+        brush8.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::ToolTipBase, brush8);
+        palette.setBrush(QPalette::Active, QPalette::ToolTipText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Button, brush1);
+        palette.setBrush(QPalette::Inactive, QPalette::Light, brush2);
+        palette.setBrush(QPalette::Inactive, QPalette::Midlight, brush3);
+        palette.setBrush(QPalette::Inactive, QPalette::Dark, brush4);
+        palette.setBrush(QPalette::Inactive, QPalette::Mid, brush5);
+        palette.setBrush(QPalette::Inactive, QPalette::Text, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::BrightText, brush6);
+        palette.setBrush(QPalette::Inactive, QPalette::ButtonText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Base, brush6);
+        palette.setBrush(QPalette::Inactive, QPalette::Window, brush1);
+        palette.setBrush(QPalette::Inactive, QPalette::Shadow, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::AlternateBase, brush7);
+        palette.setBrush(QPalette::Inactive, QPalette::ToolTipBase, brush8);
+        palette.setBrush(QPalette::Inactive, QPalette::ToolTipText, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush4);
+        palette.setBrush(QPalette::Disabled, QPalette::Button, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Light, brush2);
+        palette.setBrush(QPalette::Disabled, QPalette::Midlight, brush3);
+        palette.setBrush(QPalette::Disabled, QPalette::Dark, brush4);
+        palette.setBrush(QPalette::Disabled, QPalette::Mid, brush5);
+        palette.setBrush(QPalette::Disabled, QPalette::Text, brush4);
+        palette.setBrush(QPalette::Disabled, QPalette::BrightText, brush6);
+        palette.setBrush(QPalette::Disabled, QPalette::ButtonText, brush4);
+        palette.setBrush(QPalette::Disabled, QPalette::Base, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Window, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Shadow, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::AlternateBase, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::ToolTipBase, brush8);
+        palette.setBrush(QPalette::Disabled, QPalette::ToolTipText, brush);
+        textMonthlyCompensation->setPalette(palette);
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(10, 190, 111, 16));
+        textSocialSecurityNumber = new QPlainTextEdit(centralWidget);
+        textSocialSecurityNumber->setObjectName(QStringLiteral("textSocialSecurityNumber"));
+        textSocialSecurityNumber->setGeometry(QRect(10, 150, 104, 31));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(10, 130, 111, 16));
+        textDoneHours = new QPlainTextEdit(centralWidget);
+        textDoneHours->setObjectName(QStringLiteral("textDoneHours"));
+        textDoneHours->setGeometry(QRect(150, 300, 104, 31));
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(150, 280, 111, 16));
+        textHourlyCompensation = new QPlainTextEdit(centralWidget);
+        textHourlyCompensation->setObjectName(QStringLiteral("textHourlyCompensation"));
+        textHourlyCompensation->setGeometry(QRect(10, 300, 104, 31));
+        label_6 = new QLabel(centralWidget);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(10, 280, 111, 16));
+        textRealizedOutcome = new QPlainTextEdit(centralWidget);
+        textRealizedOutcome->setObjectName(QStringLiteral("textRealizedOutcome"));
+        textRealizedOutcome->setGeometry(QRect(10, 380, 104, 31));
+        label_8 = new QLabel(centralWidget);
+        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setGeometry(QRect(10, 360, 111, 16));
+        label_9 = new QLabel(centralWidget);
+        label_9->setObjectName(QStringLiteral("label_9"));
+        label_9->setGeometry(QRect(10, 340, 111, 16));
+        label_10 = new QLabel(centralWidget);
+        label_10->setObjectName(QStringLiteral("label_10"));
+        label_10->setGeometry(QRect(10, 260, 111, 16));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(20, 430, 75, 23));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(110, 430, 75, 23));
+        checkBox = new QCheckBox(centralWidget);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+        checkBox->setGeometry(QRect(150, 380, 121, 17));
+        MainWindow->setCentralWidget(centralWidget);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 412, 21));
+        MainWindow->setMenuBar(menuBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Employee Database", 0));
+        comboBox->clear();
+        comboBox->insertItems(0, QStringList()
+         << QApplication::translate("MainWindow", "Monthly paid employee", 0)
+         << QApplication::translate("MainWindow", "Hourly paid employee", 0)
+         << QApplication::translate("MainWindow", "Salesman", 0)
+        );
+        label->setText(QApplication::translate("MainWindow", "Select employee", 0));
+        label_2->setText(QApplication::translate("MainWindow", "Employee name", 0));
+        label_3->setText(QApplication::translate("MainWindow", "Salary/month", 0));
+        label_4->setText(QApplication::translate("MainWindow", "Social Security Number", 0));
+        label_5->setText(QApplication::translate("MainWindow", "Done hours", 0));
+        label_6->setText(QApplication::translate("MainWindow", "Salary/hour", 0));
+        label_8->setText(QApplication::translate("MainWindow", "Bonus %", 0));
+        label_9->setText(QApplication::translate("MainWindow", "Salesman:", 0));
+        label_10->setText(QApplication::translate("MainWindow", "Hourly paid employee:", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Save", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Print", 0));
+        checkBox->setText(QApplication::translate("MainWindow", "Outcome Claim", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
