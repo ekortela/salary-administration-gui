4ad31b01ca34578acc1048deff06f27b9565f7d7
Aapo Keskimölö <aapokesk@gmail.com>
2016-08-12 20:24:56 +0300

    Added project files for QT creator with qmake

--- SalaryAdministrationGui/gui/EmployeeView.cpp
+++
@@ -1,9 +0,0 @@
-#include "EmployeeView.h"
-
-
-void EmployeeView::printEmployeeInfoAll(vector<Employee*> model) {
-	for (unsigned int i = 0; model.size() > i; i++) {
-		model[i]->printInfo();
-		cout << "\n";
-	}
-}
